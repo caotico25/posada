2e43f2ee94f16c257389f569d8a0e1e4ee8357a7
unknown <caos@.(none)>
2014-05-19 17:09:58 +0200

    Foro acabado

---
+++ application/views/foro/nuevo_comentario.php
@@ -0,0 +1,11 @@
+<section>
+    <h2>Crear un nuevo comentario.</h2>
+    <p>Recuerda seguir las normas del foro. Si las incumples, tu comentario será eliminado.</p>
+    <article>
+        <?= form_open('foro/comentarios/nuevo_comentario') ?>
+            <textarea name="contenido" rows="8" cols="40"></textarea>
+            <input type="hidden" name="id_post" value="<?= $id_post ?>" id="id_post"/>
+            <input type="submit" name="enviar" value="Enviar" id="enviar"/>
+        <?= form_close() ?>
+    </article>
+</section>
