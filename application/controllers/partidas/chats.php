e5bc8b5ddccb0676e1f2e7d3e4ef8b052c703e37
unknown <caos@.(none)>
2015-03-25 22:50:25 +0100

    dgfdgnh

--- application/controllers/partidas/chats.php
+++ application/controllers/partidas/chats.php
@@ -7,13 +7,13 @@ class Chats extends CI_Controller
      */
     function insertar_mensaje()
     {
-        /*$partida = $this->input->post('partida');
+        $partida = $this->input->post('partida');
         
         $this->Chat->insertar_mensaje($this->input->post());
         
         $data['mensajes'] = array_reverse($this->Chat->obtener_mensajes($partida));
         
-        $this->load->view('partidas/chat', $data);*/
+        $this->load->view('partidas/chat', $data, TRUE);
         
         return "<script>alert('Enro en servidor bien')</script>";
     }
