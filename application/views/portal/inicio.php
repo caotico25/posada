8c466aa4c19373022cdde31f64721283a3a72a2c
unknown <caos@.(none)>
2015-04-09 17:06:20 +0200

    fgbnfdfbd

--- application/views/portal/inicio.php
+++ application/views/portal/inicio.php
@@ -27,34 +27,38 @@
             <p><?= $partida['descripcion'] ?></p>
             <span><?= obtener_estado($partida['estado']) ?></span>
             
-            <?php if (participa(obtener_id(), $partida['id'])): ?>
-                
-                <?php if (es_master(obtener_id(), $partida['id'])): ?>
+            <?php if (logueado()): ?>
+            
+                <?php if (participa(obtener_id(), $partida['id'])): ?>
                     
-                    <?= form_open('partidas/partidas/partida_master', array('target' => '_blank')) ?>
-                        <input type="hidden" name="id_partida" value="<?= $partida['id'] ?>" id="id_partida"/>
-                        <input type="submit" name="iniciar" value="Iniciar partida" id="iniciar"/>
-                    <?= form_close() ?>
+                    <?php if (es_master(obtener_id(), $partida['id'])): ?>
+                        
+                        <?= form_open('partidas/partidas/partida_master', array('target' => '_blank')) ?>
+                            <input type="hidden" name="id_partida" value="<?= $partida['id'] ?>" id="id_partida"/>
+                            <input type="submit" name="iniciar" value="Iniciar partida" id="iniciar"/>
+                        <?= form_close() ?>
+                        
+                    <?php else: ?>
+                        
+                        <?= form_open('partidas/partidas/partida_jugador', array('target' => '_blank')) ?>
+                            <input type="hidden" name="id_partida" value="<?= $partida['id'] ?>" id="id_partida"/>
+                            <input type="submit" name="entrar" value="Jugar" id="entrar"/>
+                        <?= form_close() ?>
+                        
+                    <?php endif ?>
                     
                 <?php else: ?>
-                    
-                    <?= form_open('partidas/partidas/partida_jugador', array('target' => '_blank')) ?>
-                        <input type="hidden" name="id_partida" value="<?= $partida['id'] ?>" id="id_partida"/>
-                        <input type="submit" name="entrar" value="Jugar" id="entrar"/>
-                    <?= form_close() ?>
+                
+                    <?php if ($partida['estado'] == 2): ?>
+                        <?= form_open('partidas/partidas/unirse_a_partida') ?>
+                            <input type="hidden" name="id_partida" value="<?= $partida['id'] ?>" id="id_partida"/>
+                            <input type="hidden" name="tipo_juego" value="<?= $partida['tipo_juego'] ?>" id="tipo_juego"/>
+                            <input type="submit" name="enviar" value="Únete a la aventura" id="enviar"/>
+                        <?= form_close() ?>
+                    <?php endif ?>
                     
                 <?php endif ?>
-                
-            <?php else: ?>
             
-                <?php if ($partida['estado'] == 2): ?>
-                    <?= form_open('partidas/partidas/unirse_a_partida') ?>
-                        <input type="hidden" name="id_partida" value="<?= $partida['id'] ?>" id="id_partida"/>
-                        <input type="hidden" name="tipo_juego" value="<?= $partida['tipo_juego'] ?>" id="tipo_juego"/>
-                        <input type="submit" name="enviar" value="Únete a la aventura" id="enviar"/>
-                    <?= form_close() ?>
-                <?php endif ?>
-                
             <?php endif ?>
                 
         </article>
