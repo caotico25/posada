c7a257a140ce0e4e2288122b7cb24a36ad0cf545
unknown <caos@.(none)>
2015-04-09 20:30:38 +0200

    Arreglado chat

--- application/views/partidas/chat.php
+++ application/views/partidas/chat.php
@@ -1,16 +1,23 @@
 <?php foreach ($mensajes as $mensaje): ?>
-<?php
-    $nombre = nombre_personaje($mensaje['partida'], $mensaje['jugador']);
-    
-    if ($nombre == '' || $nombre == NULL)
-    {
-        $nombre = 'Master';
-    }
-?>
+
     <?php if (trim($mensaje['mensaje']) != ''): ?>
-    <div>
-        <span><?= $nombre ?>:</span>
-        <span><?= $mensaje['mensaje'] ?></span><br />
-    </div>
+        <div>
+            
+            <?php 
+            
+                $nombre = '';
+                if (es_admin(obtener_id()))
+                {
+                    $nombre = 'Master';
+                }
+                else
+                {
+                    $nombre = nombre_personaje($mensaje['partida'], $mensaje['jugador']);
+                }
+            ?>
+            
+            <span><?= $nombre ?>:</span>
+            <span><?= $mensaje['mensaje'] ?></span><br />
+        </div>
     <?php endif ?>
 <?php endforeach ?>
