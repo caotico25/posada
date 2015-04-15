8af10ab57ba7461f1f4794e4ff22875a083ab2d9
unknown <caos@.(none)>
2015-04-15 23:52:51 +0200

    ethsrbrbb

--- application/views/admin/foro/alta_seccion.php
+++ application/views/admin/foro/alta_seccion.php
@@ -2,7 +2,7 @@
     <h2>CREAR UNA NUEVA SECCION</h2>
     <article>
     <?= validation_errors() ?>
-	<?= form_open('admin/foros/crear_seccion') ?>
+	<?= form_open('admin/foros/crear_seccion', array('class' => 'formu')) ?>
 		<label for="nombre">Nombre de la nueva sección:</label><input type="text" name="nombre" value="" id="nombre"/>
 		<label for="descripcion">Descripción:</label><textarea name="descripcion" rows="8" cols="40"></textarea>
 		<input type="submit" name="enviar" value="Crear sección" id="enviar"/>
