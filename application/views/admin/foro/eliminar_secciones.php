e0456743e57c6c5c06804bcb0ee402631dba3ee2
unknown <caos@.(none)>
2015-04-16 17:28:20 +0200

    fhbxdh

--- application/views/admin/foro/eliminar_secciones.php
+++ application/views/admin/foro/eliminar_secciones.php
@@ -12,5 +12,17 @@
                 </td>
             </tr>
         <?php endforeach ?>
-    </table></article>
+    </table>
+    <ul>
+        <?php foreach ($secciones as $seccion): ?>
+            <li>
+                <p><?= $seccion['titulo'] ?></p>
+                <?= form_open('admin/foros/eliminar_seccion') ?>
+                    <?= form_hidden('id_seccion', $seccion['id']) ?>
+                    <?= form_submit('enviar', 'Eliminar') ?>
+                <?= form_close() ?>
+            </li>
+        <?php endforeach ?>
+    </ul>
+    </article>
 </section>
