6a786af06860cafe3c953b42acc8e28642374e40
unknown <caos@.(none)>
2015-04-16 18:33:58 +0200

    estilos nueva partida

--- application/views/partidas/nueva_partida.php
+++ application/views/partidas/nueva_partida.php
@@ -4,13 +4,14 @@
 <section>
     <h2>CREA UNA NUEVA AVENTURA</h2>
     <article>
-        <?= form_open('partidas/partidas/nueva_partida', array('name' => 'nueva_partida', 'id' => 'nueva_partida')) ?>
+        <?= form_open('partidas/partidas/nueva_partida', array('name' => 'nueva_partida', 'id' => 'nueva_partida', 'class' => 'registro')) ?>
+            <?= form_error('nombre') ?>
             <label for="nombre">Nombre: </label>
             <input type="text" name="nombre" id="nombre"/>
-            <?= form_error('nombre') ?>
+            <?= form_error('descripcion') ?>
             <label for="descripcion">Descripción: </label>
             <textarea name="descripcion" id="descripcion" rows="8" cols="40"></textarea>
-            <?= form_error('descripcion') ?>
+            <?= form_error('tipo_juego') ?>
             <label for="tipo_juego">Formato de juego: </label>
             <select name="tipo_juego" id="tipo_juego">
                 <option>------</option>
@@ -18,7 +19,7 @@
                     <option value="<?= $tipo_juego['id'] ?>"><?= $tipo_juego['nombre'] ?></option>
                 <?php endforeach ?>
             </select>
-            <?= form_error('tipo_juego') ?>
+            <?= form_error('estado') ?>
             <label for="estado">Estado de la partida: </label>
             <select name="estado" id="estado">
                 <option>------</option>
@@ -26,7 +27,7 @@
                     <option value="<?= $estado['id'] ?>"><?= $estado['estado'] ?></option>
                 <?php endforeach ?>
             </select>
-            <?= form_error('estado') ?>
+            
             <input type="hidden" name="master" value="<?= obtener_id() ?>" id="master"/>
             <input type="submit" name="enviar" value="Crear" id="enviar"/>
         <?= form_close() ?>
