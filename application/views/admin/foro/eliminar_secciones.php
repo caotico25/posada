5205cc493df5f23e4f353265644f3782a1cf8353
unknown <caos@.(none)>
2015-04-16 17:30:02 +0200

    nbfghdh

--- application/views/admin/foro/eliminar_secciones.php
+++ application/views/admin/foro/eliminar_secciones.php
@@ -1,28 +1,16 @@
 <section class="admin">
-    <h2>ELIMINAR SECCIONES</h2><article>
-    <table>
-        <?php foreach ($secciones as $seccion): ?>
-            <tr>
-                <td><?= $seccion['titulo'] ?></td>
-                <td>
+    <h2>ELIMINAR SECCIONES</h2>
+    <article>
+        <ul>
+            <?php foreach ($secciones as $seccion): ?>
+                <li>
+                    <p><?= $seccion['titulo'] ?></p>
                     <?= form_open('admin/foros/eliminar_seccion') ?>
                         <?= form_hidden('id_seccion', $seccion['id']) ?>
                         <?= form_submit('enviar', 'Eliminar') ?>
                     <?= form_close() ?>
-                </td>
-            </tr>
-        <?php endforeach ?>
-    </table>
-    <ul>
-        <?php foreach ($secciones as $seccion): ?>
-            <li>
-                <p><?= $seccion['titulo'] ?></p>
-                <?= form_open('admin/foros/eliminar_seccion') ?>
-                    <?= form_hidden('id_seccion', $seccion['id']) ?>
-                    <?= form_submit('enviar', 'Eliminar') ?>
-                <?= form_close() ?>
-            </li>
-        <?php endforeach ?>
-    </ul>
+                </li>
+            <?php endforeach ?>
+        </ul>
     </article>
 </section>
