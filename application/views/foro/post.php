7b275e165cacec4cdc1433bf2917d8f2250c1d53
unknown <caos@.(none)>
2015-04-18 01:12:35 +0200

    fdsgfdgs

--- application/views/foro/post.php
+++ application/views/foro/post.php
@@ -5,14 +5,14 @@
     <h2><?= $post['titulo'] ?></h2>
     <article class="post">
         <p><?= nl2br($post['contenido']) ?></p>
-        <span>Por <?= obtener_autor($post['autor']) ?> - <?= $post['fecha'] ?></span>
+        <span>Por: <?= obtener_autor($post['autor']) ?> - <?= $post['fecha'] ?></span>
     </article>
     
     <!-- BOTON NUEVO COMENTARIO -->
     <?php if (count($comentarios) > 0): ?>
         <?php foreach ($comentarios as $comentario): ?>
             <article class="comentario">
-                <h3><?= obtener_autor($comentario['autor']) ?></h3>
+                <h3>Por: <?= obtener_autor($comentario['autor']) ?></h3>
                 <p><?= nl2br($comentario['contenido']) ?></p>
                 <span><?= $post['fecha'] ?></span>
             </article>
