19215dc0cb6e109d42caa1bb1fe7bc3bbe7377ac
unknown <caos@.(none)>
2015-04-18 03:23:07 +0200

    fdgfgdgd

---
+++ application/controllers/ayuda/ayudas.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Ayudas extends CI_Controller
+{
+    /*
+     * 
+     */
+    function index()
+    {
+        redir_sitio('ayuda/ayuda');
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+    
---
+++ application/views/ayuda/ayuda.php
@@ -0,0 +1,11 @@
+<div id="migas">
+    <a href="<?= base_url('portal/inicio') ?>">Inicio</a> <span>></span> <a href="<?= base_url('ayuda/ayudas') ?>">Ayuda</a>
+</div>
+<section id="ayuda">
+    <h2>AYUDAS</h2>
+        <article>
+            <h3>GUIA: Cómo usar la ficha de personaje.</h3>
+            <p>Mostramos como usar la ficha de personaje!</p>
+            <span></span>
+        </article>
+</section>
