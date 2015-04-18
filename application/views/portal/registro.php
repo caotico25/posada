5e083d39ec65d05d5eaa1a2cbed1689d5799aa76
unknown <caos@.(none)>
2015-04-18 21:05:32 +0200

    accebilidad AA 1

--- application/views/portal/registro.php
+++ application/views/portal/registro.php
@@ -6,11 +6,11 @@
     <article>
     <?= form_open('portal/inicio/login', array('class' => 'registro')) ?>
         <?= form_error('usuario_log') ?>
-        <label for="usuario_log">Usuario:</label>
-        <input type="text" name="usuario_log"/>
+        <label for="usuario_logg">Usuario:</label>
+        <input type="text" name="usuario_log" id="usuario_logg" />
         <?= form_error('passwd') ?>
-        <label for="passwd">Contraseña</label>
-        <input type="password" name="passwd" />
+        <label for="passwd_logg">Contraseña</label>
+        <input type="password" name="passwd" id="passwd_logg" />
         <input type="submit" name="login" value="Login" />
     <?= form_close() ?>
     </article>
@@ -21,16 +21,16 @@
     <?= form_open('portal/inicio/alta', array('class' => 'registro')) ?>
         <?= form_error('usuario') ?>
         <label for="usuario">Usuario:</label>
-        <input type="text" name="usuario" />
+        <input type="text" name="usuario" id="usuario" />
         <?= form_error('email') ?>
         <label for="email">Correo electrónico:</label>
         <input type="email" name="email" />
         <?= form_error('password') ?>
         <label for="password">Contraseña</label>
-        <input type="password" name="password" />
+        <input type="password" name="password" id="password" />
         <?= form_error('password_confirm') ?>
         <label for="password_confirm">Contraseña</label>
-        <input type="password" name="password_confirm" />
+        <input type="password" name="password_confirm" id="password_confirm" />
         <input type="submit" name="registro" value="Registrate" />
     <?= form_close() ?>
     </article>
