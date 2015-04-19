e35c8c0154cd0a4a4a3af739426986dad11259b3
unknown <caos@.(none)>
2015-04-19 18:54:41 +0200

    psdkdsjfjsffbf

--- application/views/ayuda/ayuda.php
+++ application/views/ayuda/ayuda.php
@@ -4,8 +4,20 @@
 <section id="ayudas">
     <h2>AYUDAS</h2>
         <article>
-            <h3>GUIA: Cómo usar la ficha de personaje.</h3>
-            <p>Mostramos como usar la ficha de personaje!</p>
-            <span></span>
+            <h3><u>GUIA: Cómo usar la ficha de personaje.</u></h3>
+            <p>
+                Usar tu ficha de personaje es muy sencillo! <br />
+                
+                Primero, expliquemos las partes de tu ficha de personaje:
+            </p>
+            <p>
+                - El chat: con él podrás comunicarte con el resto de participantes. Para enviar un mensaje tan solo pulsa enter. <br /><br />
+                - El tablero de dados: con él podrás lanzar todos los dados que necesites en cualquier momento. <br /><br />
+                - Los datos de tu personaje: aquí aparecerán los datos de tu personaje, que deberás rellenar tú mismo. Los cambios se almacenan automáticamente, 
+                asi que no te preocupes de perder información. <br />
+                Para ver los campos relativos a alguna de las secciones, tan solo pulsa sobre la sección que deseas ver.
+                
+            </p>
+            <span>Por: Admin</span>
         </article>
 </section>
